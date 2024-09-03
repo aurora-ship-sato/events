b98078461dda030dac6d4bb52991e78f5e78d9e3
aurora-ship-sato <k-sato@shipinc.biz>
2024-09-03 14:24:41 +0900

    Update plugin.php

--- custom-events-templates/plugin.php
+++ custom-events-templates/plugin.php
@@ -26,3 +26,38 @@ function custom_events_load_templates($template)
 }
 
 add_filter('template_include', 'custom_events_load_templates');
+
+// プラグインが有効化されたときのフック
+register_activation_hook(__FILE__, 'cet_activate_plugin');
+function cet_activate_plugin()
+{
+  // JSONファイルのパス
+  $json_file_path = plugin_dir_path(__FILE__) . 'acf-export-2024-09-03.json';
+
+  // JSONファイルの内容を取得
+  $json_content = file_get_contents($json_file_path);
+
+  if ($json_content) {
+    $field_groups = json_decode($json_content, true);
+
+    if (!empty($field_groups)) {
+      foreach ($field_groups as $group) {
+        // ACFのインポート関数を使用してカスタムフィールドグループをインポート
+        acf_import_field_group($group);
+      }
+    }
+  }
+}
+
+// プラグインが無効化されたときのフック
+register_deactivation_hook(__FILE__, 'cet_deactivate_plugin');
+function cet_deactivate_plugin()
+{
+  // 削除するフィールドグループのキー（JSONファイルから取得）
+  $field_group_key = 'group_5f694267a7c7c';
+
+  if (function_exists('acf_delete_field_group')) {
+    acf_delete_field_group($field_group_key);
+  }
+}
+
